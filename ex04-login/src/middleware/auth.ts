1ea95868b3343abbf9183aa6a31e1cac7824418a
Rachata Singkhet <singkhet1@gmail.com>
2025-06-08 17:38:04 +0700

    Create auth.ts

---
+++ ex04-login/src/middleware/auth.ts
@@ -0,0 +1,36 @@
+import { expressjwt } from "express-jwt";
+import { env } from "../config/env";
+import { Request, Response, NextFunction } from "express";
+import { JWTUserPayload } from "../interfaces/auth.interfaces";
+
+declare global {
+    namespace Express {
+        interface Request {
+            auth?: JWTUserPayload;
+        }
+    }
+}
+
+export const requireLogin = expressjwt({
+    secret: env.JWT_SECRET,
+    algorithms: ["HS256"],
+    requestProperty: "auth", 
+});
+
+
+interface Role {
+    role: "user" | "admin";
+}
+
+export const checkRole = (role: Role) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const auth = req.auth as JWTUserPayload;
+
+        if (auth && auth.role === role.role) {
+            next();
+        } else {
+            console.log(`❌ Role mismatched: Required ${role.role}, got ${auth?.role}`);
+            return res.status(403).json({ message: "Forbidden: Insufficient role" });
+        }
+    };
+};
