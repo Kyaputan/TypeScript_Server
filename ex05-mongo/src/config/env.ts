3475a1b4067d3827bd8a0ecdcf779e18c305b1f6
Rachata Singkhet <singkhet1@gmail.com>
2025-06-09 19:23:14 +0700

    Update env.ts

--- ex05-mongo/src/config/env.ts
+++ ex05-mongo/src/config/env.ts
@@ -7,7 +7,7 @@ dotenv.config({ path: path.resolve(__dirname, '../.env') });
 export const env: EnvConfig = {
   PORT: process.env.PORT || "error",
   JWT_SECRET: process.env.JWT_SECRET || "error",
-  DATABASE_URL: process.env.DATABASE_URL || "error",
+  DATABASE_URL: process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/error",
   NODE_ENV: process.env.NODE_ENV || "development",
   BCRYPT_SALT_ROUNDS: Number(process.env.BCRYPT_SALT_ROUNDS) || 10,
 };
